832f74acda4ead94d5b872f61eef6849a28771ae
sarah-svg <kjskillen@gmail.com>
2021-02-03 13:50:45 -0800

    news/articleDisplay

---
+++ src/components/news/ArticlesDisplay.jsx
@@ -0,0 +1,26 @@
+// Article - Display an articles title, author, and description
+// Articles - Display a list of Article components
+// Search - Display a form used to search articles by term
+////////
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ArticleDisplay = ({ title, author, description }) => (
+  <div data-testid="display">
+    <h2>{title}</h2>
+    <h3>{author}</h3>
+    <span>{description}</span>
+
+  </div>
+ 
+
+
+);
+ArticleDisplay.propTypes = {
+  title: PropTypes.sting.isRequired,
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired
+};
+
+
+export default ArticleDisplay;
